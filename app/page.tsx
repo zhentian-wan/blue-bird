d9f9a2d958aff52f804483341d4cdd67182aff67
zhentian-wan <answer881215@gmail.com>
2023-07-30 11:14:30 +0300

    login / types

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from "next/headers";
-import AuthButton from './auth-button';
+import AuthButtonServer from './auth-button.server';
+import { redirect } from 'next/navigation';
+
 
 export default async function Home() {
-  const supabase = createServerComponentClient({cookies})
+  const supabase = createServerComponentClient<Database>({cookies})
+  const {data: {session}} = await supabase.auth.getSession()
+  if (!session) {
+    redirect('/login')
+  }
   const {data: tweets} = await supabase.from("tweets").select()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButton />
+      <AuthButtonServer />
       <pre>
         {JSON.stringify(tweets, null, 2)}
       </pre>
---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+// keep refershing cookie after expired
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient<Database>({ req, res });
+  await supabase.auth.getSession();
+  return res;
+}
